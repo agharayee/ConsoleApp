8646964ee5da29ab5f7eece6ad27ead203efcb50
agharaye emmanuel <agharayeemmanuel@hotmail.com>
2021-02-24 22:27:33 +0100

    small refactored

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SimpleAtmApp.classes;
 
 namespace SimpleAtmApp
 {
@@ -6,116 +7,26 @@ namespace SimpleAtmApp
     {
         static void Main(string[] args)
         {
-            var openingBalance = 5000;
-            var flag = false;
-            while (flag != true)
+            var Transaction = new Transaction();
+            decimal openingBalance = 5000;
+            while (true)
             {
-                Console.WriteLine("Welcome to Simple bank Atm");
-                System.Console.WriteLine($"Press 1 to withdraw");
-                System.Console.WriteLine($"Press 2 to Deposit");
-                System.Console.WriteLine($"Press 3 to check balance");
-                System.Console.WriteLine($"Press 4 to remove your card");
-                //System.Console.WriteLine($"Thanks for using Simple Bank Atm");
-
+                Transaction.Display();
+                
                 var input = int.Parse(Console.ReadLine());
-                if (input > 4 && input < 1)
-                {
-                    System.Console.WriteLine($"Please Enter a valid operation");
-                    Console.WriteLine("Welcome to Simple bank Atm");
-                    System.Console.WriteLine($"Press 1 to withdraw");
-                    System.Console.WriteLine($"Press 2 to Deposit");
-                    System.Console.WriteLine($"Press 3 to check balance");
-                    System.Console.WriteLine($"Press 4 to remove your card");
-                }
-                else if (input == 1)
-                {
-                    System.Console.WriteLine($"Please enter the amount you want to withdraw");
-                    var amountToWithdraw = int.Parse(Console.ReadLine());
-                    while (amountToWithdraw > openingBalance)
-                    {
-                        try
-                        {
-                            System.Console.WriteLine($"Insufficent fund please try again");
-                            System.Console.WriteLine($"Please enter the amount you want to withdraw");
-                            amountToWithdraw = int.Parse(Console.ReadLine());
-                        }
-                        catch (Exception ex)
-                        {
-                            System.Console.WriteLine($"{ex.Message}");
-                        }
+                //if console.readline is deposit run this method
+                Transaction.Withdraw(openingBalance, input);
+                
+                //else if it is withdrawal run this methof
+                Transaction.Deposit(openingBalance, input);
 
-                    }
-                    System.Console.WriteLine("Please take your cash");
-                    openingBalance -= amountToWithdraw;
-                    System.Console.WriteLine($"Your balance is {openingBalance}");
-                    Console.WriteLine("Do you want to perform another task? Press 1 for YES and 2 for No");
-                    var exit = int.Parse(Console.ReadLine());
+                //else if it is check balance run check balance method
+                Transaction.CheckBalance(openingBalance, input);
 
-                    if (exit >= 2 && exit < 0)
-                    {
-                        if (exit == 1)
-                        {
-                            continue;
-                        }
-                        if (exit == 2)
-                        {
-                            System.Console.WriteLine("Thanks for using Simple bank ATM");
-                            break;
-                        }
-                    }
-                }
-                else if (input == 2)
-                {
-                    System.Console.WriteLine($"How much do you want to deposit into your account?");
-                    try{
-                        var deposit = int.Parse(Console.ReadLine());
-                        openingBalance += deposit;
-                    }
-                    catch(Exception ex){
-                        System.Console.WriteLine($"{ex.Message}");
-                    } 
-                    System.Console.WriteLine($"Your new balance is {openingBalance}");
-                    Console.WriteLine("Do you want to perform another task? Press 1 for YES and 2 for No");
-                    var exit = int.Parse(Console.ReadLine());
-                    if (exit >= 2 && exit < 0)
-                    {
-                        if (exit == 1)
-                        {
-                            continue;
-                        }
-                        if (exit == 2)
-                        {
-                            System.Console.WriteLine("Thanks for using Simple bank ATM");
-                            break;
-                        }
-                    }
-                }
-                else if (input == 3)
-                {
-                    System.Console.WriteLine($"Your Account balance is {openingBalance}");
-                    Console.WriteLine("Do you want to perform another task? Press 1 for YES and 2 for No");
-                    var exit = int.Parse(Console.ReadLine());
-                   if (exit >= 2 && exit < 0)
-                    {
-                        if (exit == 1)
-                        {
-                            continue;
-                        }
-                        if (exit == 2)
-                        {
-                            System.Console.WriteLine("Thanks for using Simple bank ATM");
-                            break;
-                        }
-                    }
-                }
-
-                else if (input == 4)
-                {
-                    Environment.Exit(-1);
-
-                }
+                //else if it quit, quit the running console
+                Transaction.Exit(input);
+               
             }
-
         }
     }
 }
--- classes/Transaction.cs
+++ classes/Transaction.cs
@@ -4,38 +4,84 @@ namespace SimpleAtmApp.classes
 {
     public class Transaction
     {
-        decimal openingBalance {get; set;} = 5000;
+        decimal openingBalance { get; set; } = 5000;
         public void Display()
         {
-
+            Console.WriteLine("Welcome to Simple bank Atm");
+            System.Console.WriteLine($"Press 1 to withdraw");
+            System.Console.WriteLine($"Press 2 to Deposit");
+            System.Console.WriteLine($"Press 3 to check balance");
+            System.Console.WriteLine($"Press 4 to remove your card");
+            //System.Console.WriteLine($"Thanks for using Simple Bank Atm");
         }
-        public void Withdraw(){
-                {
-                    System.Console.WriteLine($"Please enter the amount you want to withdraw");
-                    var amountToWithdraw = decimal.Parse(Console.ReadLine());
+        public void Withdraw(decimal openingBalance, int input)
+        {
+            if (input == 1)
+            {
+                System.Console.WriteLine($"Please enter the amount you want to withdraw");
+                    var amountToWithdraw = int.Parse(Console.ReadLine());
                     while (amountToWithdraw > openingBalance)
                     {
-                        System.Console.WriteLine($"Insufficent fund please try again");
-                        System.Console.WriteLine($"Please enter the amount you want to withdraw");
-                        amountToWithdraw = decimal.Parse(Console.ReadLine());
+                        try
+                        {
+                            System.Console.WriteLine($"Insufficent fund please try again");
+                            System.Console.WriteLine($"Please enter the amount you want to withdraw");
+                            amountToWithdraw = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Console.WriteLine($"{ex.Message}");
+                        }
+
                     }
                     System.Console.WriteLine("Please take your cash");
                     openingBalance -= amountToWithdraw;
                     System.Console.WriteLine($"Your balance is {openingBalance}");
-                    // Console.WriteLine("Do you want to perform another task? Press 1 for YES and 2 for No");
-                    // 
+            }
+            // 
+            // 
 
-                    // if (exit == 1)
-                    // {
-                    //     continue;
-                    // }
-                    // if (exit == 2)
-                    // {
-                    //     System.Console.WriteLine("Thanks for using Simple bank ATM");
-                    //     break;
-                    // }
-                }
-                
+            // if (exit == 1)
+            // {
+            //     continue;
+            // }
+            // if (exit == 2)
+            // {
+            //     System.Console.WriteLine("Thanks for using Simple bank ATM");
+            //     break;
+            // }
         }
+
+        public void Deposit(decimal openingBalance, int input)
+        {
+            if (input == 2)
+            {
+                System.Console.WriteLine($"How much do you want to deposit into your account?");
+                try
+                {
+                    var deposit = decimal.Parse(Console.ReadLine());
+                    openingBalance += deposit;
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"{ex.Message}");
+                }
+                System.Console.WriteLine($"Your new balance is {openingBalance}");
+            }
+
+        }
+
+        public void CheckBalance(decimal openingBalance, int input)
+        {
+            if (input == 3)
+            {
+                System.Console.WriteLine($"Your Account balance is {openingBalance}");
+            }
+        }
+        public void Exit(int input)
+        {
+            Environment.Exit(-1);
+        }
+
     }
 }
